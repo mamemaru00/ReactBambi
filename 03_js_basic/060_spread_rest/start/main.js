f80048447656c8424145e7e3c2121c57f2dbdfd6
mamemaru00 <k.shouhei0711@gmail.com>
2022-11-23 21:24:43 +0900

    03まで終了

--- 03_js_basic/060_spread_rest/start/main.js
+++ 03_js_basic/060_spread_rest/start/main.js
@@ -6,20 +6,22 @@ console.log(result);
 
 let arr1 = [1, 2, 3];
 let arr2 = [4, 5, 6];
-let newArr = [...arr1];
-
-console.log(newArr2);
+let newArr = [...arr1, 10,  ...arr2];
+let newArr1 = arr1;
+newArr1.push(4)
+console.log(newArr);
 
 const obj = {
   name: "Tom",
   age: 22,
 };
 const newObj = { ...obj };
+newObj.name = 'john';
 
-console.log(newObj);
+console.log(newObj, obj);
 
 const restA = (...argA) => console.log(argA);
-restA(1, 3, 4)
+restA(1, 3)
 
 const restB = (n, ...argB) => console.log(argB);
 restB(1, 3, 4)
